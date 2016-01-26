3103cc248c28ffd8d6d1458435cfb0bc0a2ef2a2
Lloyd Russell <llerussell@gmail.com>
2016-01-26 17:32:14 +0000

    gui size

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'GUI.ui'
 #
-# Created: Tue Jan 26 17:23:07 2016
+# Created: Tue Jan 26 17:31:47 2016
 #      by: PyQt5 UI code generator 5.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,7 +12,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(730, 800)
+        MainWindow.resize(720, 820)
         MainWindow.setMinimumSize(QtCore.QSize(720, 0))
         MainWindow.setStyleSheet("font-size: 8pt;")
         MainWindow.setAnimated(True)
@@ -262,7 +262,7 @@ class Ui_MainWindow(object):
         self.scrollArea.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
         self.scrollArea.setObjectName("scrollArea")
         self.scrollAreaWidgetContents = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, -837, 609, 1294))
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, -837, 599, 1294))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
